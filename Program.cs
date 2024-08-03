bff8d10fe67bf2de4771b21abec528eb2e5b7c29
uzkid <ibowik694@gmail.com>
2024-08-03 22:05:40 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Hello, Please Enter your Name: ");
+string Name  = Console.ReadLine();
+System.Console.WriteLine($"Welcome, {Name}");
+
+int KESCHAsAge = 1;
+System.Console.Write("Now, Enter Your Age Please: ");
+string UsersAgeAsString = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int UAAint = Convert.ToInt32(UsersAgeAsString);
+System.Console.WriteLine($"Succesfully Converted! {UsersAgeAsString}");
+int AgeDifference = UAAint - KESCHAsAge;
+
+System.Console.WriteLine($"The Difference Between Your and KESCHAs Ages is {AgeDifference}");
